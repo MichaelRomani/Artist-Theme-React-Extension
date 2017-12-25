723244933db4eede699eec31fc8398e10b33747d
Mike Romani <mr4227@gmail.com>
2017-12-25 16:04:15 -0500

    build second render

--- src/index2.js
+++ src/index2.js
@@ -1,23 +1,23 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import './index.css'
-// import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App2 from './App2'
 
-// // Get the element to prepend our app to from https://www.google.com.
-// // This could be a specific element on a website or something more general like `document.body`.
-// const viewport = document.getElementById('viewport')
+// Get the element to prepend our app to from https://www.google.com.
+// This could be a specific element on a website or something more general like `document.body`.
+const viewport = document.getElementById('viewport')
 
-// // Create a div to render the App component to.
-// const app = document.createElement('div')
+// Create a div to render the App component to.
+const app = document.createElement('div')
 
-// // Set the app element's id to `root`.
-// // This name is the same as the element that create-react-app renders to by default
-// // so it will work on the development server too.
-// app.id = 'root2'
+// Set the app element's id to `root`.
+// This name is the same as the element that create-react-app renders to by default
+// so it will work on the development server too.
+app.id = 'root2'
 
-// // Prepend the App to the viewport element in production if it exists on the page.
-// // You could also use `appendChild` depending on your needs.
-// if (viewport) viewport.prepend(app)
+// Prepend the App to the viewport element in production if it exists on the page.
+// You could also use `appendChild` depending on your needs.
+if (viewport) viewport.append(app)
 
-// // Render the App.
-// ReactDOM.render(<App />, document.getElementById('hplogo'))
+// Render the App.
+ReactDOM.render(<App2 />, document.getElementById('root2'))
