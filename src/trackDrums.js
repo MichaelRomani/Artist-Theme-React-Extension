0b538b603e30e0097d9547bf4635034fcd0a3cc1
Mike Romani <mr4227@gmail.com>
2018-01-17 17:57:23 -0500

    audio added and plays when expected

--- src/trackDrums.js
+++ src/trackDrums.js
@@ -4,6 +4,7 @@ import ReactHowler from 'react-howler'
 import { connect } from 'react-redux'
 import DrakeDrum from './audio/audioDrakeDrum.js'
 import DipsetDrum from './audio/audio'
+import ChooseASongPrompt from './audio/chooseASongPrompt';
 
 class DrumTrack extends Component {
   constructor(props) {
@@ -24,8 +25,11 @@ class DrumTrack extends Component {
       case 'Drake-0-100':
         song = DrakeDrum.one
         break
+      case '':
+        song = ChooseASongPrompt.one
+        break
       default:
-        song = 'nothing'
+        song = ''
         break
     }
     if (!this.props.beat && this.didMount) this.audioDrums.stop()
